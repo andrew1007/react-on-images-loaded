edbfba9984401067d3ab9525c36ff0813539942a
Andrew <yueh.andrew@gmail.com>
2022-03-21 16:59:18 -0700

    parcel

---
+++ example/src/loaders/images_with_component.tsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import images from './images';
+import OnImagesLoaded from 'react-on-images-loaded';
+import LoadingSpinner from '../loading_spinner';
+import PropTypes from 'prop-types'
+export default class ImagesWithComponent extends Component {
+  static propTypes = {
+    showError: PropTypes.bool
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    };
+  }
+
+  images() {
+    const imgs = images.map((url, idx) => (
+      <div key={idx} className='image'>
+        <img src={url + '?' + new Date().getTime()} className='image' />
+      </div>
+    ));
+    return (
+      <div className={this.state.loaded ? 'hidden-false' : 'hidden-true'}>
+        {imgs}
+      </div>
+    );
+  }
+
+  onLoadedHandler() {
+    this.setState({ loaded: true });
+  }
+
+  handleTimeout() {
+    this.props.showError();
+    alert('hit timeout. this is the onTimeout function being run and is mounting normally now');
+    this.setState({ loaded: true });
+  }
+
+  render() {
+    return (
+      <OnImagesLoaded
+        onLoaded={() => this.onLoadedHandler()}
+        onTimeout={() => this.handleTimeout()}
+        timeout={70000}
+      >
+        {this.state.loaded ? null : <LoadingSpinner />}
+        {this.images()}
+      </OnImagesLoaded>
+    );
+  }
+}
